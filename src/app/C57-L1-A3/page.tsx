5745aa202da6adc0628557ae432088e44ad81ceb
mohammad asad <asadkhan41376@gmail.com>
2026-04-17 15:43:15 +0530

    add new activity C57-L1-A3

---
+++ src/app/C57-L1-A3/page.tsx
@@ -0,0 +1,12 @@
+import LayoutC57L1A3 from '@/layout-C57-L1-A3'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <LayoutC57L1A3/>
+    </div>
+  )
+}
+
+export default Page
